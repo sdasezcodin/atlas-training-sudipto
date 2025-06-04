02bda81d252e58500b0df9aab8d1b90eda6f1d07
eZcodiN <sdas.ezcodin@gmail.com>
2025-06-04 16:39:59 +0530

    Enumerations

---
+++ java/Task016.java
@@ -0,0 +1,17 @@
+package Enumerations;
+
+// Define an enum named 'Color'
+enum Color {
+    red, blue, green, yellow
+}
+
+// Main class to demonstrate enums
+public class Task016 {
+    public static void main(String[] args) {
+        // Assign a value from the enum to a variable
+        Color c1 = Color.yellow;
+
+        // Print the enum value
+        System.out.println(c1);  // Output: yellow
+    }
+}
