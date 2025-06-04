1ef3cc63d8367f58ffc910ca6a4e5d19b88cdadf
eZcodiN <sdas.ezcodin@gmail.com>
2025-06-04 15:10:13 +0530

    Task 1
    Write a Java program to print “Hello guys!!!!” and “We are learning Java”.

---
+++ java/Task001.java
@@ -0,0 +1,19 @@
+// Class to display messages
+class Display {
+
+    // Method to print welcome messages
+    void output() {
+        System.out.println("Hello guys!!!!");
+        System.out.println("We are learning Java");
+    }
+}
+
+public class Task001 {
+    public static void main(String[] args) {
+        // Create an object of the Display class
+        Display display = new Display();
+
+        // Call the output method to print the messages
+        display.output();
+    }
+}
