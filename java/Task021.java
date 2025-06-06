880f47afb15bf6d2aba89c438f4c0028411e0aed
eZcodiN <sdas.ezcodin@gmail.com>
2025-06-06 12:22:42 +0530

    initialize an array and traverse it using a loop

---
+++ java/Task021.java
@@ -0,0 +1,14 @@
+public class Task021 {
+    public static void main(String[] args) {
+
+        // initializing array
+        int[] arr = {1, 2, 3, 4, 5};
+
+        // size of array
+        int n = arr.length;
+
+        // traversing array
+        for (int j : arr) System.out.print(j + " ");
+    }
+}
+
