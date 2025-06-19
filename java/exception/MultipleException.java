e9e9d4e5b386760c587471cb71d5215a3edf3401
eZcodiN <sdas.ezcodin@gmail.com>
2025-06-19 09:50:56 +0530

    DAY NINE

---
+++ java/exception/MultipleException.java
@@ -0,0 +1,16 @@
+public class MultipleException {
+
+        public static void main(String args[]) {
+            try {
+                int a[] = new int[2];
+                int b = 0;
+                int c = 1/b;
+                System.out.println("Access element three :" + a[3]);
+            }
+            catch (ArrayIndexOutOfBoundsException | ArithmeticException e) {
+                System.out.println("Exception thrown  :" + e);
+            }
+            System.out.println("Out of the block");
+        }
+
+}
---
+++ java/exception/ThrowException.java
@@ -0,0 +1,24 @@
+public class ThrowException {
+    // Demonstrating how to throw an exception
+
+        // Method declares it may throw IllegalAccessException
+        static void fun() throws IllegalAccessException {
+            System.out.println("Inside fun().");
+            // Throwing an exception manually using 'throw'
+            throw new IllegalAccessException("demo");
+        }
+
+        public static void main(String args[]) {
+            try {
+                fun();  // Calling method that may throw exception
+
+                // method2();   → Can be used to simulate ArrayIndexOutOfBoundsException
+                // method3();   → Can be used to simulate FileNotFoundException
+
+            } catch (IllegalAccessException e) {
+                System.out.println("Caught in main.");
+            }
+        }
+    }
+
+
