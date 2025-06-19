57a5761a3de0af49455804e0bd94573ca441a3b4
eZcodiN <sdas.ezcodin@gmail.com>
2025-06-19 10:30:31 +0530

    Accessing Members of Inner and Outer Class (day9)

---
+++ java/Task042.java
@@ -0,0 +1,21 @@
+class OuterClass {
+    int x = 10;
+    static class InnerClass {
+        int y = 5;
+    }
+}
+public class Task042 {
+    public static void main(String[] args) {
+      //  OuterClass myOuter = new OuterClass();
+       // OuterClass myouter;
+
+        OuterClass.InnerClass myInner = new OuterClass.InnerClass();
+
+
+        System.out.println(myInner.y );
+    }
+}
+
+
+
+
