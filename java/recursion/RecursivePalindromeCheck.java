febb173a1fc52ad6954c1f026a112d3ef9cd9054
eZcodiN <sdas.ezcodin@gmail.com>
2025-07-09 10:12:43 +0530

    DAY FOURTEEN

---
+++ java/recursion/RecursivePalindromeCheck.java
@@ -0,0 +1,23 @@
+public class RecursivePalindromeCheck {
+    public static boolean isPalindrome(String str, int start, int end) {
+        if (start >= end) {
+            return true;
+        }
+        if (str.charAt(start) != str.charAt(end)) {
+            return false;
+        }
+        return isPalindrome(str, start + 1, end - 1);
+    }
+
+    public static void main(String[] args) {
+        String input = "madam";
+
+        boolean result = isPalindrome(input, 0, input.length() - 1);
+
+        if (result) {
+            System.out.println("\"" + input + "\" is a palindrome.");
+        } else {
+            System.out.println("\"" + input + "\" is not a palindrome.");
+        }
+    }
+}
