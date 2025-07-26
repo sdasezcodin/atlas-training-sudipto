e33ed181ed9948dcc6e8c9c51cde093c806e788c
eZcodiN <sdas.ezcodin@gmail.com>
2025-07-26 21:22:13 +0530

    DAY TWENTY

---
+++ java/principles/Studenttt.java
@@ -0,0 +1,25 @@
+public class Studenttt {
+
+    // Method to handle student registration details
+    public void registrationDetails() {
+        // not implemented yet
+    }
+
+    // Method to calculate marks
+    public void marksCalc() {
+        // not implemented yet
+    }
+
+    // Method to calculate fees
+    public void feesCalc() {
+        // not implemented yet
+    }
+
+    public static void main(String[] args) {
+        // You can create an object here if needed for testing
+        Studenttt s = new Studenttt();
+        // s.registrationDetails();
+        // s.marksCalc();
+        // s.feesCalc();
+    }
+}
