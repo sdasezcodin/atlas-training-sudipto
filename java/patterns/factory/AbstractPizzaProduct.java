6f95dddca642acb77e03b9f722147f604bce3671
eZcodiN <sdas.ezcodin@gmail.com>
2025-08-07 12:42:00 +0530

    Delete java/patterns/factory directory

--- java/patterns/factory/AbstractPizzaProduct.java
+++
@@ -1,6 +0,0 @@
-package com.factory;
-
-public interface AbstractPizzaProduct {
-    void bake();
-}
-
--- java/patterns/factory/ConcretePepperoniPizzaProduct.java
+++
@@ -1,7 +0,0 @@
-package com.factory;
-
-public class ConcretePepperoniPizzaProduct implements AbstractPizzaProduct {
-    public void bake() {
-        System.out.println("Baking Pepperoni Pizza");
-    }
-}
